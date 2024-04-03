96b775e4849f8c4827c2b1dddd28f931cc24e351
TNinh39 <dangninh020@gmail.com>
2024-04-03 07:33:50 +0700

    KhoiTaoFormHoaDon

--- QLKS/frmInHoaDon.cs
+++ QLKS/frmInHoaDon.cs
@@ -15,7 +15,7 @@ namespace QLKS
         {
             InitializeComponent();
         }
-        //hello
+        //KhoiTao
 
         public string MaHoaDon{ get; set; }
         public string MaNhanVien { get; set; }
